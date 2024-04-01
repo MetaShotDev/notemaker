66933d3917ed46ae9316f10bc85d08a71f0a3508
unniznd <au6695@gmail.com>
2024-04-01 19:12:21 +0530

    db updated

--- backend/prod_settings.py
+++ backend/prod_settings.py
@@ -13,6 +13,13 @@ ALLOWED_HOSTS = [
 ]
 
 DATABASES = {
-    'default': dj_database_url.config(default=os.getenv("DB_URL"), conn_max_age=600)
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv("DB_NAME"),
+        'USER': os.getenv("DB_USER"),
+        'PASSWORD': os.getenv("DB_PASSWORD"),
+        'HOST': os.getenv("DB_HOST"),
+        'PORT': os.getenv("DB_PORT"),
+    }
 }
 
